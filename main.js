6554273e3a1d0c31856d02b205e6d372dad5c427
kimon-satan <simon@simonkatan.co.uk>
2016-07-25 16:57:39 +0100

    stuck on miter

--- main.js
+++ main.js
@@ -28,17 +28,17 @@ canvas.addEventListener("touchstart", function (e) {
 camera = new THREE.Camera();
 camera.position.z = 1;
 
-var numPoints = 100;
+var numPoints = 10;
 
 scene = new THREE.Scene();
 
 var mshape = new THREE.Shape();
-mshape.moveTo( -.5, 0 , 0);
+mshape.moveTo( -200., 0 , 0);
 
-for(var i = 0; i < 100; i++)
+for(var i = 0; i < numPoints; i++)
 {
-	var incr = 1.0/100.;
-	mshape.lineTo( -.5 + (i + 1) * incr, Math.sin(incr * i  * Math.PI * 6.) * .05, 0);
+	var incr = 1000.0/numPoints;
+	mshape.lineTo( -200 + (i + 1) * incr, Math.sin(incr * i  * Math.PI * 6.) * 30, 0);
 }
 
 var points = mshape.extractAllPoints();
@@ -52,11 +52,13 @@ var miters = new Float32Array( numPoints * 2);
 var lineDist = new Float32Array( numPoints * 2);
 var normals = new Float32Array( numPoints * 2 * 2);
 var indexArray = new Uint16Array( (numPoints - 1)  * 6);
+var colors = new Float32Array( numPoints * 6);
 
 var c = 0;
 
 for(var i = 0; i < numPoints ; i++)
 {
+
 
 	vertices[i * 6 + 0] = points.shape[i].x;
 	vertices[i * 6 + 1] = points.shape[i].y;
@@ -67,17 +69,22 @@ for(var i = 0; i < numPoints ; i++)
 	vertices[i * 6 + 4] = vertices[i * 6 + 1];
 	vertices[i * 6 + 5] = 0.;
 
-	//on either side 
-	//we use this to make a varying
-	miters[i * 2] = 1.;
-	miters[i * 2 + 1] = -1.; 
+	colors[i * 6 + 0] = Math.random();
+	colors[i * 6 + 1] = Math.random();
+	colors[i * 6 + 2] = Math.random();
 
-	indexArray[c++] = i * 2;
-	indexArray[c++] = i * 2 + 1;
-	indexArray[c++] = i * 2 + 3;
-	indexArray[c++] = i * 2 + 0;
+	colors[i * 6 + 3] = colors[i * 6 + 0];
+	colors[i * 6 + 4] = colors[i * 6 + 1];
+	colors[i * 6 + 5] = colors[i * 6 + 2];
+
+
+
 	indexArray[c++] = i * 2 + 2;
+	indexArray[c++] = i * 2 + 1;
+	indexArray[c++] = i * 2 + 0;
 	indexArray[c++] = i * 2 + 3;
+	indexArray[c++] = i * 2 + 1;
+	indexArray[c++] = i * 2 + 2;
 
 	lineDist[i * 2] = i/numPoints;
 	lineDist[i * 2 + 1] = i/numPoints;
@@ -103,14 +110,51 @@ for(var i = 0; i < numPoints ; i++)
 		normal.y = vertices[i*6 +1] - vertices[ni*6 +1];
 	}
 
+	//TODO optimise .. tidy up
+
+	var p0 = new THREE.Vector2(vertices[i * 6], vertices[i*6+1]);
+	var p1 = new THREE.Vector2(vertices[(i+1) * 6], vertices[(i+1)*6+1]);
+	var p2 = new THREE.Vector2(vertices[(i+2) * 6], vertices[(i+2)*6+1]);
+
+	var a = new THREE.Vector2();
+	var b = new THREE.Vector2();
+
+	a.subVectors(p2,p1);
+	a.normalize();
+	b.subVectors(p1, p0)
+	b.normalize();
+
+	var tang = new THREE.Vector2();
+	tang.addVectors(a,b);
+	tang.normalize();
+
+	var miter = new THREE.Vector2( -tang.y, tang.x );
+	miter.normalize();
+
+	console.log(miter.dot(normal));
+
+	//on either side 
+	//we use this to make a varying
+	var l = miter.dot(normal);
+
+
+
+	miters[(i+1) * 2] = l;
+	miters[(i+1) * 2 + 1] = -l; 
+
+
 	normal.normalize();
 
 	normals[i * 4 + 0] = -normal.y;
 	normals[i * 4 + 1] = normal.x;
-	normals[i * 4 + 2] = normal.y;
-	normals[i * 4 + 3] = -normal.x;
+	normals[i * 4 + 2] = -normal.y;
+	normals[i * 4 + 3] = normal.x;
+
+
 
 }
+
+
 
 // itemSize = 3 because there are 3 values (components) per vertex
 geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
@@ -118,6 +162,7 @@ geometry.addAttribute( 'miter', new THREE.BufferAttribute( miters, 1 ) );
 geometry.addAttribute( 'line_dist', new THREE.BufferAttribute( lineDist, 1 ) );
 geometry.addAttribute( 'n_normal', new THREE.BufferAttribute( normals, 2 ) );
 geometry.addAttribute('index', new THREE.BufferAttribute( indexArray, 1));
+geometry.addAttribute('color', new THREE.BufferAttribute( colors, 3));
 
 var uniforms = {
 	time:       { value: 1.0 },
@@ -133,9 +178,9 @@ uniforms.resolution.value.y = renderer.domElement.height;
 
 var material = new THREE.ShaderMaterial( {
 	uniforms: uniforms,
-	side: THREE.DoubleSide,
 	vertexShader: document.getElementById( 'vertexShader' ).textContent,
-	fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+	fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+
 } );
 
 //var pts = new THREE.Points(geometry, material);
@@ -151,6 +196,7 @@ var startTime = new Date().getTime();
 var ellapsedTime = 0;
 
 
+
 function render() {
 
 	ellapsedTime = (new Date().getTime() - startTime) * 0.001;
@@ -159,12 +205,23 @@ function render() {
 
 	//console.log(ellapsedTime);
 	
+	//var gl = renderer.context;
+	//var ext = gl.getExtension("EXT_blend_minmax");
+  	//gl.enable(gl.BLEND);
+
+	//gl.blendEquation(ext.MAX_EXT);
+  	//gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ZERO);
+  	
+  	//gl.blendColor(0,0,0,1);
+
+
 	renderer.render( scene, camera );
 	requestAnimationFrame( render );
 	
 }
 
 render();
+
 
 /*----------------------------------------GUI----------------------------------------------*/
 

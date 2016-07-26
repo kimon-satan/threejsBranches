d397388b3340d0ed0e17050fe7dbf41c013dbaba
kimon-satan <simon@simonkatan.co.uk>
2016-07-26 18:12:35 +0100

    mousedrawing its fucked up

--- main.js
+++ main.js
@@ -9,12 +9,43 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 var canvas;
 var mousePos = new THREE.Vector2(0,0);
+var cbranch = null;
+var mousedown = false;
 
 canvas = renderer.domElement;
 
+canvas.addEventListener("mousedown", function (e) {
+      
+    if(!mousedown){
+	    mousePos.set(e.clientX/width, e.clientY/height);
+	    mousePos.add(new THREE.Vector2(-.5 , -.5)).multiply(new THREE.Vector2(2.0 * width/height, -2.0));
+
+	    cbranch = new Branch(mousePos);
+	    branches.push(cbranch);
+	    scene.add(cbranch.mesh);
+	    mousedown = true;
+	}
+ }, false);
+
+
+
 canvas.addEventListener("mousemove", function (e) {
+     
+     if(mousedown){
+	    mousePos.set(e.clientX/width, e.clientY/height);
+	    mousePos.add(new THREE.Vector2(-.5 , -.5)).multiply(new THREE.Vector2(2.0 * width/height, -2.0));
+
+   		cbranch.updateVertices(mousePos);
+	}
+
+ }, false);
+
+canvas.addEventListener("mouseup", function (e) {
         
-    mousePos.set(e.clientX/width, e.clientY/height);
+	mousePos.set(e.clientX/width, e.clientY/height);
+	mousePos.add(new THREE.Vector2(-.5 , -.5)).multiply(new THREE.Vector2(2.0 * width/height, -2.0));
+    cbranch = null;
+    mousedown = false;
 
  }, false);
 
@@ -47,15 +78,14 @@ uniforms.resolution.value.y = renderer.domElement.height;
 
 //////////////////////////////////////////////////////////////////BRANCH//////////////////////////////////////////////////////////////////
 
-function Branch(sp, dir){
+function Branch(sp){
 
 	
 	this.maxPoints = 300;
-	this.numPoints = 10; 
+	this.numPoints = 1; 
 	this.startPos = sp;
 	this.endPos = new THREE.Vector2().copy(sp);
 	this.step = 0.001 + Math.random() * 0.009;
-	this.incr = dir.normalize().multiplyScalar(this.step);
 
 	//attributes
 	this.vertices = new Float32Array( this.maxPoints * 6);
@@ -77,11 +107,6 @@ function Branch(sp, dir){
 	this.createGeometry = function()
 	{
 
-		//only needs to be called once
-
-		var p = new THREE.Vector2().copy(this.startPos);
-		var norm = new THREE.Vector2(-this.incr.y, this.incr.x).normalize();
-
 		for(var i = 0; i < this.maxPoints; i++)
 		{
 			this.indexes[i*6] = i * 2 + 2;
@@ -91,97 +116,23 @@ function Branch(sp, dir){
 			this.indexes[i*6+4] = i * 2 + 1;
 			this.indexes[i*6+5] = i * 2 + 2;
 
-			//NB. will need to change if noise is variable
-			var n = noise.simplex2((i+1) * this.step * 5. , this.seed * 13.35433 ) * this.noise_mul * Math.sin(i/this.maxPoints * Math.PI);
-
-			this.vertices[i * 6 + 0] = p.x + norm.x * n;
-			this.vertices[i * 6 + 1] = p.y + norm.y * n;
-			this.vertices[i * 6 + 2] = 0.;
-
-			//a copy
-			this.vertices[i * 6 + 3] = this.vertices[i * 6 + 0];
-			this.vertices[i * 6 + 4] = this.vertices[i * 6 + 1];
-			this.vertices[i * 6 + 5] = 0.;
-
-			p.add(this.incr);
-
+			for(var j = 0; j < 6; j ++)
+			{
+				this.vertices[i * 6 + j] = 0;
+			}
 		}
 
-		//now calculate the mitres 
+		this.vertices[0] = this.startPos.x; ///p.x + norm.x * n;
+		this.vertices[1] = this.startPos.y; //p.y + norm.y * n;
+		this.vertices[2] = 0.;
 
-		for(var i = 0; i < this.maxPoints; i++)
-		{
+		//a copy
+		this.vertices[3] = this.vertices[0];
+		this.vertices[4] = this.vertices[1];
+		this.vertices[5] = 0.;
 
-			var pi = i - 1;
-			var ni = i + 1;
 
-			var p0 = new THREE.Vector2(this.vertices[pi * 6], this.vertices[pi*6+1]);
-			var p1 = new THREE.Vector2(this.vertices[i * 6], this.vertices[i*6+1]);
-			var p2 = new THREE.Vector2(this.vertices[ni * 6], this.vertices[ni*6+1]);
-
-			var a = new THREE.Vector2();
-			var b = new THREE.Vector2();
-
-			a.subVectors(p1, p0)
-			a.normalize();
-			b.subVectors(p2,p1);
-			b.normalize();
-
-			var normal = new THREE.Vector2(-a.y,a.x);
-
-			//for the ends
-
-			if(i == this.maxPoints -1 )
-			{
-				this.miters[i * 4] = normal.x;
-				this.miters[i * 4 + 1] = normal.y; 
-				this.miters[i * 4 + 2] = normal.x;
-				this.miters[i * 4 + 3] = normal.y; 
-				this.miter_dims[i * 2] = 1.0;
-				this.miter_dims[i * 2 + 1] = -1.0;
-				
-			}
-			else if(i == 0)
-			{
-				//construct normal using the following segment
-				
-				this.miters[i * 4] = -b.y;
-				this.miters[i * 4 + 1] = b.x; 
-				this.miters[i * 4 + 2] = -b.y;
-				this.miters[i * 4 + 3] = b.x;  
-				this.miter_dims[i * 2] = 1.0;
-				this.miter_dims[i * 2 + 1] = -1.0;
-			
-			}
-			else
-			{
-
-				//all other points
-
-				var tang = new THREE.Vector2();
-				tang.addVectors(a,b);
-				tang.normalize();
-
-				var miter = new THREE.Vector2( -tang.y, tang.x );
-				miter.normalize();
-
-				//length of miter on either side
-				var l = miter.dot(normal);
-
-				this.miters[i * 4] = miter.x;
-				this.miters[i * 4 + 1] = miter.y; 
-
-				this.miters[i * 4 + 2] = miter.x;
-				this.miters[i * 4 + 3] = miter.y; 
-
-				this.miter_dims[i * 2] = l;
-				this.miter_dims[i * 2 + 1] = -l; //signed to flip the vertex
-
-			}
-
-		}
-
-		this.endPos.set(p);
+		this.endPos.set(this.startPos);
 
 		this.geometry = new THREE.BufferGeometry();
 		this.geometry.dynamic = true;
@@ -194,6 +145,119 @@ function Branch(sp, dir){
 		this.geometry.addAttribute( 'line_prog', new THREE.BufferAttribute( this.line_prog, 1 ) );
 		this.geometry.addAttribute( 'miter', new THREE.BufferAttribute( this.miters, 2 ) );
 		this.geometry.addAttribute( 'miter_dims', new THREE.BufferAttribute( this.miter_dims, 1 ) );
+	}
+
+	this.updateVertices = function(pos)
+	{
+
+		//var p = new THREE.Vector2().copy(this.startPos);
+		//var norm = new THREE.Vector2(-this.incr.y, this.incr.x).normalize();
+		this.numPoints = Math.min(this.numPoints + 1, this.maxPoints);
+		if(this.numPoints == this.maxPoints)return;
+
+		var v = new THREE.Vector2().subVectors(pos, this.endPos).normalize().multiplyScalar(0.005);
+		var np = new THREE.Vector2().copy(this.endPos).add(v);
+
+		this.endPos.copy(pos);
+
+
+		this.uniforms.thickness.value = Math.max(0.001 , (this.numPoints/this.maxPoints) * 0.02);
+		var i = this.numPoints - 1;
+
+
+		//NB. will need to change if noise is variable
+		//var n = noise.simplex2((i+1) * this.step * 5. , this.seed * 13.35433 ) * this.noise_mul * Math.sin(i/this.maxPoints * Math.PI);
+
+		this.vertices[i * 6 + 0] = np.x; ///p.x + norm.x * n;
+		this.vertices[i * 6 + 1] = np.y; //p.y + norm.y * n;
+		this.vertices[i * 6 + 2] = 0.;
+
+		//a copy
+		this.vertices[i * 6 + 3] = this.vertices[i * 6 + 0];
+		this.vertices[i * 6 + 4] = this.vertices[i * 6 + 1];
+		this.vertices[i * 6 + 5] = 0.;
+
+		//p.add(this.incr);
+
+
+		if(this.numPoints < 3)return;
+
+		var ppi = i - 2;
+		var pi = i - 1;
+		
+
+		var p0 = new THREE.Vector2(this.vertices[ppi * 6], this.vertices[ppi*6+1]);
+		var p1 = new THREE.Vector2(this.vertices[pi * 6], this.vertices[pi*6+1]);
+		var p2 = new THREE.Vector2(this.vertices[i * 6], this.vertices[i*6+1]);
+
+		var a = new THREE.Vector2();
+		var b = new THREE.Vector2();
+
+		a.subVectors(p1, p0)
+		a.normalize();
+		b.subVectors(p2,p1);
+		b.normalize();
+
+		var normal = new THREE.Vector2(-a.y,a.x);
+
+		//for the ends
+
+		//make the most recent point the normal
+		this.miters[i * 4] = normal.x;
+		this.miters[i * 4 + 1] = normal.y; 
+		this.miters[i * 4 + 2] = normal.x;
+		this.miters[i * 4 + 3] = normal.y; 
+		this.miter_dims[i * 2] = 1.0;
+		this.miter_dims[i * 2 + 1] = -1.0;
+		
+		
+	
+		if(this.numPoints == 3)
+		{
+			//construct first normal using the following segment
+			
+			this.miters[0] = -b.y;
+			this.miters[1] = b.x; 
+			this.miters[2] = -b.y;
+			this.miters[3] = b.x;  
+			this.miter_dims[0] = 1.0;
+			this.miter_dims[1] = -1.0;
+		
+		}
+	
+
+		//all other points
+
+		var tang = new THREE.Vector2();
+		tang.addVectors(a,b);
+		tang.normalize();
+
+		var miter = new THREE.Vector2( -tang.y, tang.x );
+		miter.normalize();
+
+		//length of miter on either side
+		var l = miter.dot(normal);
+
+		this.miters[pi * 4] = miter.x;
+		this.miters[pi * 4 + 1] = miter.y; 
+
+		this.miters[pi * 4 + 2] = miter.x;
+		this.miters[pi * 4 + 3] = miter.y; 
+
+		this.miter_dims[pi * 2] = l;
+		this.miter_dims[pi * 2 + 1] = -l; //signed to flip the vertex
+
+		
+
+		if(this.numPoints < this.maxPoints)this.recalLPs();
+		this.geometry.setDrawRange (0, this.numPoints * 6); 
+		this.geometry.attributes.position.needsUpdate = true;
+
+		this.geometry.attributes.line_prog.needsUpdate = true;
+		this.geometry.attributes.miter.needsUpdate = true;
+		this.geometry.attributes.miter_dims.needsUpdate = true;
+
+
 	}
 
 
@@ -217,7 +281,7 @@ function Branch(sp, dir){
 	}
 
 	this.createGeometry();
-	this.recalLPs();
+	
 
 	for(var u in uniforms)
 	{
@@ -238,15 +302,9 @@ function Branch(sp, dir){
 
 	this.update = function(){
 
-		this.numPoints = Math.min(this.numPoints + 1, this.maxPoints);
-		this.uniforms.thickness.value = Math.max(0.001 , (this.numPoints/this.maxPoints) * 0.02);
-		if(this.numPoints < this.maxPoints)this.recalLPs();
-		this.geometry.setDrawRange (0, this.numPoints * 6); 
-		this.geometry.attributes.position.needsUpdate = true;
+		
+		
 
-		this.geometry.attributes.line_prog.needsUpdate = true;
-		this.geometry.attributes.miter.needsUpdate = true;
-		this.geometry.attributes.miter_dims.needsUpdate = true;
 	}
 
 
@@ -258,18 +316,17 @@ function Branch(sp, dir){
 
 
 var branches = [];
-var numBranches = 100;
+var numBranches = 1;
 
-var sp = new THREE.Vector2(0,0);
+//var sp = new THREE.Vector2(0,0);
 
-for (var i = 0; i < numBranches; i++)
-{
-	var l = 0.1 + Math.random() * 0.6;
-	var d = new THREE.Vector2(Math.random(),Math.random()).sub(new THREE.Vector2(.5,.5)).normalize().multiplyScalar(l * 2.);
+// for (var i = 0; i < numBranches; i++)
+// {
+// 	var l = 0.1 + Math.random() * 0.6;
+// 	var d = new THREE.Vector2(Math.random(),Math.random()).sub(new THREE.Vector2(.5,.5)).normalize().multiplyScalar(l * 2.);
 
- 	branches.push(new Branch(sp,d));
- 	scene.add(branches[i].mesh);
-}
+ 
+// }
 
 
 
@@ -285,20 +342,8 @@ function render() {
 
 	//console.log(ellapsedTime);
 
-	for (var i = 0; i < numBranches; i++)
-	{
-		branches[i].update();
-	}
+
 	
-	//var gl = renderer.context;
-	//var ext = gl.getExtension("EXT_blend_minmax");
-  	//gl.enable(gl.BLEND);
-
-	//gl.blendEquationSeparate(ext.MAX_EXT, ext.MAX_EXT);
-  	//gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ZERO);
-  	
-  	//gl.blendColor(0,0,0,1);
-
 
 	renderer.render( scene, camera );
 	requestAnimationFrame( render );
